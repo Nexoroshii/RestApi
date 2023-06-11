8bc3e4f8bb0e763402069d3c139ab3fb4bc89c0e
3rdKosme <96114647+3rdKosme@users.noreply.github.com>
2023-06-11 23:51:00 +0300

    realize REST API

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const path = require('path');
+const { v4 } = require('uuid');
 const app = express();
+
+let CONTACTS = [
+    {
+        id: v4(),
+        name: 'Egor',
+        value: '80(44) 111-22-33',
+        marked: false,
+    },
+];
+
+app.use(express.json());
+
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json(CONTACTS);
+});
+
+app.post('/api/contacts', (req, res) => {
+    const contact = { ...req.body, id: v4(), marked: false };
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
+});
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter((c) => c.id !== req.params.id);
+    res.status(200).json({ message: 'Contact was deleted' });
+});
+
+app.put('/api/contacts/:id', (req, res) => {
+    const index = CONTACTS.findIndex((c) => c.id === req.params.id);
+    CONTACTS[index] = req.body;
+    res.json(CONTACTS[index]);
+});
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 app.get('*', (req, res) =>
--- client/frontend.js
+++ client/frontend.js
@@ -3,18 +3,12 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 createApp({
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: '',
             },
-            contacts: [
-                {
-                    id: 1,
-                    name: 'Egor',
-                    value: '80(44) 111-22-33',
-                    marked: false,
-                },
-            ],
+            contacts: [],
         };
     },
     computed: {
@@ -23,18 +17,50 @@ createApp({
         },
     },
     methods: {
-        createContact() {
+        async createContact() {
             const { ...contact } = this.form;
-            this.contacts.push({ ...contact, id: Date.now(), marked: false });
+
+            const newContact = await request('/api/contacts', 'POST', contact);
+            this.contacts.push(newContact);
 
             this.form.name = this.form.value = '';
         },
-        markContact(id) {
+        async markContact(id) {
             const contact = this.contacts.find((c) => c.id === id);
-            contact.marked = true;
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true,
+            });
+            contact.marked = updated.marked;
         },
-        removeContact(id) {
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE');
             this.contacts = this.contacts.filter((c) => c.id !== id);
         },
     },
+    async mounted() {
+        this.loading = true;
+        this.contacts = await request('/api/contacts');
+        this.loading = false;
+    },
 }).mount('#app');
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {};
+        let body;
+
+        if (data) {
+            headers['Content-Type'] = 'application/json';
+            body = JSON.stringify(data);
+        }
+        const response = await fetch(url, {
+            method,
+            headers,
+            body,
+        });
+        return await response.json();
+    } catch (e) {
+        console.warn('Error!', e.message);
+    }
+}
